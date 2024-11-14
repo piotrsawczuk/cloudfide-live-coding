592ec2e52c6f133bc43c08e6460bd869e941d633
Piotr Sawczuk <p.sawczuk@exlabs.pl>
2024-11-14 13:21:37 +0100

    Added controller method to analyze cryptocurrency data.

--- src/cryptocurrency-analyzer/cryptocurrency-analyzer.controller.ts
+++ src/cryptocurrency-analyzer/cryptocurrency-analyzer.controller.ts
@@ -1,4 +1,23 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
+import { CryptocurrencyAnalyzerService } from './cryptocurrency-analyzer.service';
+import { AnalyzePeriodRequest } from './model/analyze-period.request';
+import { CryptocurrencyChange } from './model/cryptocurrency-change.model';
 
 @Controller('cryptocurrency-analyzer')
-export class CryptocurrencyAnalyzerController {}
+export class CryptocurrencyAnalyzerController {
+  constructor(
+    private readonly cryptocurrencyAnalyzerService: CryptocurrencyAnalyzerService,
+  ) {}
+
+  @Post()
+  async analyzeCryptocurrencyData(
+    @Body() request: AnalyzePeriodRequest,
+  ): Promise<CryptocurrencyChange[]> {
+    const { symbol, dateFrom, dateTo } = request;
+    return this.cryptocurrencyAnalyzerService.analyzePeriod(
+      symbol,
+      new Date(dateFrom),
+      new Date(dateTo),
+    );
+  }
+}
---
+++ src/cryptocurrency-analyzer/model/analyze-period.request.ts
@@ -0,0 +1,5 @@
+export interface AnalyzePeriodRequest {
+  symbol: string;
+  dateFrom: string;
+  dateTo: string;
+}
