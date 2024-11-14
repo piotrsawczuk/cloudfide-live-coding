154edbc4c77c15d170f55b38005b9235297cc8c4
Piotr Sawczuk <p.sawczuk@exlabs.pl>
2024-11-14 14:09:51 +0100

    Added binance method test without mock to check on real data - shouldn't be used in real world scenario as it can block records.

--- src/binance/binance.service.spec.ts
+++ src/binance/binance.service.spec.ts
@@ -16,7 +16,17 @@ describe('BinanceService', () => {
     expect(service).toBeDefined();
   });
 
-  it('should return data', async () => {
+  it('should return actual data', async () => {
+    expect(
+      await service.historicalMarketData(
+        'ETHBTC',
+        new Date('2024-11-01'),
+        new Date('2024-11-14'),
+      ),
+    ).toHaveLength(500); // Default binance length - normally should not use this kind of expect, but it's just for check
+  });
+
+  it('should return mocked data', async () => {
     service.historicalMarketData = jest.fn().mockImplementation(() => []);
 
     expect(
